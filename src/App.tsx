c9b337f619748be520c5a634e04b99aea50d4b3f
Mehmet Fırat Yılmaz <firat@Mehmet-MacBook-Air.local>
2026-02-24 16:22:58 +0300

    chore : initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App () {
+ return (
+ <div >
+ <h1 >Web Tasarimi ve Programlama </h1 >
+ <h2 >LAB -1 </h2 >
+ <p>Ad Soyad : [ Mehmet Fırat Yılmaz ]</p>
+ <p> Ogrenci No: [ 230541051 ]</p>
+ </div >
+ )
+ }
+ export default App
